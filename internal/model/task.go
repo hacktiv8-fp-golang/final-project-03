19ce3646bf3c1814485512755081f02cdeffcf16
Fabianus Jericho HJS <fabianusjeriko@gmail.com>
2023-11-20 10:32:48 +0700

    Added All Task

--- internal/model/task.go
+++ internal/model/task.go
@@ -8,7 +8,7 @@ type Task struct {
 	ID          uint      `json:"id" gorm:"primaryKey" `
 	Title       string    `json:"title" gorm:"not null" valid:"required"`
 	Description string    `json:"description" gorm:"not null" valid:"required"`
-	Status      bool      `json:"status" gorm:"not null" valid:"required,in(admin|member)"`
+	Status      bool      `json:"status" gorm:"not null"`
 	UserID      uint      `json:"user_id"`
 	CategoryID  uint      `json:"category_id"`
 	CreatedAt   time.Time `json:"created_at"`
@@ -25,13 +25,33 @@ type TaskCreate struct {
 type TaskUpdate struct {
 	Title       string `json:"title" gorm:"not null" valid:"required"`
 	Description string `json:"description" gorm:"not null" valid:"required"`
-	CategoryID  uint   `json:"category_id"`
+	// CategoryID  uint   `json:"category_id"`
 }
 
 type TaskStatusUpdate struct {
-	Status bool `json:"status" gorm:"not null" valid:"required,in(admin|member)"`
+	Status bool `json:"status" gorm:"not null"`
 }
 
 type TaskCategoryUpdate struct {
 	CategoryID uint `json:"category_id"`
+}
+
+type TaskCreateResponse struct {
+	ID          uint      `json:"id" gorm:"primaryKey" `
+	Title       string    `json:"title" gorm:"not null" valid:"required"`
+	Status      bool      `json:"status" gorm:"not null"`
+	Description string    `json:"description" gorm:"not null" valid:"required"`
+	UserID      uint      `json:"user_id"`
+	CategoryID  uint      `json:"category_id"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type TaskUpdateResponse struct {
+	ID          uint      `json:"id" gorm:"primaryKey" `
+	Title       string    `json:"title" gorm:"not null" valid:"required"`
+	Description string    `json:"description" gorm:"not null" valid:"required"`
+	Status      bool      `json:"status" gorm:"not null"`
+	UserID      uint      `json:"user_id"`
+	CategoryID  uint      `json:"category_id"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
---
+++ internal/repository/task-repository.go
@@ -0,0 +1,107 @@
+package repository
+
+import (
+	"final-project-03/internal/database"
+	"final-project-03/internal/helper"
+	"final-project-03/internal/model"
+)
+
+type taskModelRepo interface {
+	CreateTask(task *model.Task) (*model.Task, helper.Error)
+	GetAllTasks() ([]*model.Task, helper.Error)
+	UpdateTask(input *model.TaskUpdate, taskID uint) (*model.Task, helper.Error)
+	UpdateStatusTask(taskStatus *model.TaskStatusUpdate, taskID uint) (*model.Task, helper.Error)
+	UpdateCategoryIdTask(input *model.TaskCategoryUpdate, taskID uint) (*model.Task, helper.Error)
+	
+	DeleteTask(taskId uint) helper.Error
+}
+
+type taskModel struct{}
+
+var TaskModel taskModelRepo = &taskModel{}
+
+func (t *taskModel) CreateTask(task *model.Task) (*model.Task, helper.Error){
+	db := database.GetDB()
+
+	err:= db.Create(&task).Error
+
+	if err != nil {
+		return nil, helper.ParseError(err)
+	}
+
+	return task, nil
+}
+
+func (t *taskModel) GetAllTasks() ([]*model.Task, helper.Error) {
+	db := database.GetDB()
+	var tasks []*model.Task
+
+	err := db.Preload("User").Find(&tasks).Error
+
+	if err != nil {
+		return nil, helper.ParseError(err)
+	}
+
+	return tasks, nil
+}
+
+func (t *taskModel) UpdateTask(input *model.TaskUpdate, taskID uint) (*model.Task, helper.Error) {
+	db := database.GetDB()
+
+	var task model.Task
+	err := db.First(&task, taskID).Error
+
+	if err != nil {
+		return nil, helper.ParseError(err)
+	}
+
+	db.Model(&task).Updates(input)
+
+	return &task, nil
+}
+
+func (t *taskModel) UpdateStatusTask(taskStatus *model.TaskStatusUpdate, taskID uint) (*model.Task, helper.Error) {
+	db := database.GetDB()
+
+	var task model.Task
+
+	err := db.First(&task, taskID).Error
+
+	if err != nil {
+		return nil, helper.ParseError(err)
+	}
+
+	db.Model(&task).Updates(taskStatus)
+
+	return &task, nil
+}
+
+func (t *taskModel) UpdateCategoryIdTask(input *model.TaskCategoryUpdate, taskID uint) (*model.Task, helper.Error) {
+	db := database.GetDB()
+
+	var task model.Task
+
+	err := db.First(&task, taskID).Error
+
+	if err != nil {
+		return nil, helper.ParseError(err)
+	}
+
+	db.Model(&task).Updates(input)
+
+	return &task, nil
+}
+
+func (t *taskModel) DeleteTask(taskId uint) helper.Error {
+	db := database.GetDB()
+
+	var task model.Task
+
+	err := db.Where("id = ?", taskId).Delete(&task).Error
+	
+	if err != nil {
+		return helper.ParseError(err)
+	}
+
+	return nil
+}
